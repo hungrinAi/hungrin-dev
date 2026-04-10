8319015a92b594820d6df1f921d206eb95509aa8
Promi <promi38@student.sust.edu>
2026-04-10 12:53:21 +0600

    fix: resolve Vercel deployment by adding vercel.json with Next.js framework config

---
+++ .next/static/chunks/src_app_insights_page_tsx_e0a9c458._.js
@@ -0,0 +1,14 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_56b6fd65._.js",
+  "static/chunks/node_modules_recharts_es6_util_0bc80dc6._.js",
+  "static/chunks/node_modules_recharts_es6_state_de4b15af._.js",
+  "static/chunks/node_modules_recharts_es6_component_3248c833._.js",
+  "static/chunks/node_modules_recharts_es6_cartesian_0571a9d4._.js",
+  "static/chunks/node_modules_recharts_es6_71e0c81a._.js",
+  "static/chunks/node_modules_6b8529af._.js",
+  "static/chunks/src_80e556ca._.js"
+],
+    source: "dynamic"
+});
